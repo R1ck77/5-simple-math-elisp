ebf0cd9dd50afecec0d19b21a16bd4bf7f7803d6
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-10-07 23:33:44 +0200

    Some refactoring

--- simple-math.el
+++ simple-math.el
@@ -2,12 +2,6 @@
 
 (defconst operations '(+ - * /))
 
-(defun sm--read-number (prompt)
-  (let ((number (read-number prompt)))
-    (if (< number 0)
-        (sm--read-number prompt)
-      number)))
-
 (defun sm--do-interpose (seq sep)
   (let* ((tmp (reverse seq))
         (result (list (first tmp)))
@@ -22,17 +16,41 @@
    ((< (length seq) 2) seq)
    (:otherwise (sm--do-interpose seq sep))))
 
+(defun sm--merge-strings (seq)
+  (reduce 'concat (interpose "\n" seq)))
+
+(defun sm--represent-result (format-string name-result-seq)
+  (mapcar (lambda (x)
+            (apply 'format format-string x))
+          name-result-seq))
+
+(defun sm--apply-operation (operation values)
+  (funcall operation
+           (first values)
+           (second values)))
+
+(defun sm--operations-results (values)
+  (mapcar (lambda (x)
+            (list (symbol-name x)
+                  (sm--apply-operation x values)))
+          operations))
+
+(defun sm--read-number (prompt)
+  (let ((number (read-number prompt)))
+    (if (< number 0)
+        (sm--read-number prompt)
+      number)))
+
+(defun sm--read-numbers ()
+  (list (sm--read-number "First number: ")
+        (sm--read-number "Second number: ")))
+
 (defun simple-math ()
   (interactive)
-  (let* ((a (sm--read-number "First number: "))
-         (b (sm--read-number "Second number: "))
-         (format-string (format "%g %%s %g = %%g" a b)))
-    (insert
-     (reduce 'concat
-             (interpose "\n" (mapcar (lambda (x)
-                                (format format-string (first x) (second x)))
-                              (mapcar (lambda (x) (list (symbol-name x)
-                                                        (funcall x a b)))
-                                      operations)))))))
+  (let ((values (sm--read-numbers)))
+   (insert
+    (sm--merge-strings
+     (sm--represent-result (apply 'format "%g %%s %g = %%g" values)
+                           (sm--operations-results values))))))
 
 (provide 'simple-math)
