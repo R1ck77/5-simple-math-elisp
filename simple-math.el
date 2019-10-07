000feb7ba883b47a6b160d5dba90924e54106893
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-10-07 21:25:19 +0200

    WIP to do some refactoring

--- simple-math.el
+++ simple-math.el
@@ -1,14 +1,6 @@
 (require 'cl)
 
-(defun sm--format (a b)
-  (format "%g + %g = %g
-%g - %g = %g
-%g * %g = %g
-%g / %g = %g"
-          a b (+ a b)
-          a b (- a b)
-          a b (* a b)
-          a b (/ a b)))
+(defconst operations '(+ - * /))
 
 (defun sm--read-number (prompt)
   (let ((number (read-number prompt)))
@@ -16,10 +8,31 @@
         (sm--read-number prompt)
       number)))
 
+(defun sm--do-interpose (seq sep)
+  (let* ((tmp (reverse seq))
+        (result (list (first tmp)))
+        (source (rest tmp)))
+    (while source
+      (setq result (cons (first source) (cons sep result)))
+      (setq source (rest source)))
+    result))
+
+(defun interpose (sep seq)
+  (cond
+   ((< (length seq) 2) seq)
+   (:otherwise (sm--do-interpose seq sep))))
+
 (defun simple-math ()
   (interactive)
-  (insert
-   (sm--format (sm--read-number "First number: ")
-               (sm--read-number "Second number: "))))
+  (let* ((a (sm--read-number "First number: "))
+         (b (sm--read-number "Second number: "))
+         (format-string (format "%g %%s %g = %%g" a b)))
+    (insert
+     (reduce 'concat
+             (interpose "\n" (mapcar (lambda (x)
+                                (format format-string (first x) (second x)))
+                              (mapcar (lambda (x) (list (symbol-name x)
+                                                        (funcall x a b)))
+                                      operations)))))))
 
 (provide 'simple-math)
